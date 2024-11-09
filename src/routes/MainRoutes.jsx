cf2282f6135f331b4222d839284c7750a86c37e9
mj-saini <manojsaini97285@gmail.com>
2024-11-09 21:19:19 +0530

    marge

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,25 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "../pages/main-website-pages/HomePage"
-import AboutPage from "../pages/main-website-pages/AboutPage"
-import Pricing from "../pages/main-website-pages/Pricing"
-import Register from "../pages/main-website-pages/Register"
-import Login from "../pages/main-website-pages/Login"
-import ContactUs from "../pages/main-website-pages/ContactUs"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/main-website-pages/HomePage";
+import AboutPage from "../pages/main-website-pages/AboutPage";
+import Pricing from "../pages/main-website-pages/Pricing";
+import Register from "../pages/main-website-pages/Register";
+import Login from "../pages/main-website-pages/Login";
+import ContactUs from "../pages/main-website-pages/ContactUs";
 
 const MainRoutes = () => {
   return (
     <div>
-        <Routes>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about-us" element={<AboutPage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/register" element={<Register />} />
         <Route path="/sign-in" element={<Login />} />
-        </Routes>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
